8f5c563774dda11a69b17b0f4a6d1bc636d6841c
force55 <force55595@gmail.com>
2024-11-03 19:30:25 +0100

    init

---
+++ resources/views/livewire/dashboard.blade.php
@@ -0,0 +1,28 @@
+<?php
+
+    use App\Models\ListeningParty;
+    use Livewire\Volt\Component;
+
+    new class extends Component {
+
+        public string $name = '';
+        public $startTime;
+
+
+        public function createListeningParty()
+        {
+
+        }
+
+        public function with()
+        {
+            return [
+                'listening_parties' => ListeningParty::all(),
+
+            ]
+    }
+    }; ?>
+
+<div>
+    hello
+</div>
